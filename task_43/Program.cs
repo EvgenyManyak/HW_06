46bfcc48714f368d7fe9b14a8ee26df799913b43
Evgeny <buriy-anarchist@inbox.ru>
2022-07-19 00:21:33 +0300

    Add task 43

---
+++ task_43/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+ 
+double x = - (b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
