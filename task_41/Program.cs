b276b6fd117399787de64c8681ab63218779ad8f
Evgeny <buriy-anarchist@inbox.ru>
2022-07-19 00:25:06 +0300

    Add task 41

---
+++ task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        {
+            count++;
+        }
+}
+Console.WriteLine($"Кол-во чисел > 0: {count}");
